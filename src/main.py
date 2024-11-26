3e207c8a8b06c2f6897ac8030ca21441bd1b049f
Faiza <salami.faiza15@gmail.com>
2024-11-25 22:53:22 -0600

    code

--- src/main.py
+++ src/main.py
@@ -1,2 +1,24 @@
 import nltk
 
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+
+
+def identify_parts_of_speech(sentence):
+    words = nltk.word_tokenize(sentence)
+
+    pos_tags = nltk.pos_tag(words)
+
+    for word, pos in pos_tags:
+        print(f"{word}: {pos}")
+
+
+
+def main():
+    sentence = input("Enter a sentence: ")
+
+    identify_parts_of_speech(sentence)
+
+
+if __name__ == "__main__":
+    main()
